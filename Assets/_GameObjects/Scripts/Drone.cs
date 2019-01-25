67ec5695a8e2c4fc6ee8a7cb389b0ec4157365c6
Fernando Paniagua <fpaniagua.unity.2018.9@gmail.com>
2019-01-25 14:00:25 +0100

    2019-01-25

---
+++ Assets/_GameObjects/Scripts/Drone.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drone : MonoBehaviour
+{
+    public float speed = 3f;//[SerializeField]
+    public Transform baliza;
+    
+    void Update()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        if (transform.position.z >= baliza.position.z) {
+            transform.Translate(Vector3.back * Time.deltaTime * speed);
+            transform.Rotate(new Vector3(0,90,0));
+        }
+    }
+}
---
+++ Assets/_GameObjects/Scripts/Esfera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Esfera : MonoBehaviour
+{
+    public GameObject drone;
+    private Drone droneScript;
+    
+    private void Start() {
+        droneScript = drone.GetComponent<Drone>();
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        droneScript.enabled = true;
+    }
+
+    private void OnTriggerExit(Collider other) {
+        droneScript.enabled = false;
+    }
+}
